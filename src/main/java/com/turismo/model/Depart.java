085df384d17fb9212e049bc760a8dd6956441527
MatiasSolisBello <matias_solis11@hotmail.com>
2020-11-16 17:55:39 -0300

    Checks

--- src/main/java/com/turismo/model/Depart.java
+++ src/main/java/com/turismo/model/Depart.java
@@ -33,6 +33,8 @@ public class Depart implements Serializable{
 	@Column
 	private String ciudad;
 	@Column
+	private String mantenimiento;
+	@Column
 	private String descripcion;
 	@Column
 	private byte[] foto;
@@ -40,75 +42,122 @@ public class Depart implements Serializable{
 	@OneToMany(mappedBy = "depart", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private Set<Reserva> reserva;
 	
+	
+
 	public Depart() {
-		
 	}
 
-	public Depart(int id_depart, int precio, String estado, String ciudad, String descripcion, byte[] foto) {
+
+
+	public Depart(int id_depart, int precio, String estado, String ciudad, String mantenimiento, String descripcion,
+			byte[] foto, Set<Reserva> reserva) {
 		super();
 		this.id_depart = id_depart;
 		this.precio = precio;
 		this.estado = estado;
 		this.ciudad = ciudad;
+		this.mantenimiento = mantenimiento;
 		this.descripcion = descripcion;
 		this.foto = foto;
+		this.reserva = reserva;
 	}
+
+
 
 	public int getId_depart() {
 		return id_depart;
 	}
 
+
+
 	public void setId_depart(int id_depart) {
 		this.id_depart = id_depart;
 	}
+
+
 
 	public int getPrecio() {
 		return precio;
 	}
 
+
+
 	public void setPrecio(int precio) {
 		this.precio = precio;
 	}
+
+
 
 	public String getEstado() {
 		return estado;
 	}
 
+
+
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
+
+
 
 	public String getCiudad() {
 		return ciudad;
 	}
 
+
+
 	public void setCiudad(String ciudad) {
 		this.ciudad = ciudad;
 	}
+
+
+
+	public String getMantenimiento() {
+		return mantenimiento;
+	}
+
+
+
+	public void setMantenimiento(String mantenimiento) {
+		this.mantenimiento = mantenimiento;
+	}
+
+
 
 	public String getDescripcion() {
 		return descripcion;
 	}
 
+
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+
+
 
 	public byte[] getFoto() {
 		return foto;
 	}
 
+
+
 	public void setFoto(byte[] foto) {
 		this.foto = foto;
 	}
+
+
 
 	public Set<Reserva> getReserva() {
 		return reserva;
 	}
 
+
+
 	public void setReserva(Set<Reserva> reserva) {
 		this.reserva = reserva;
 	}
+	
 	
 	
 	
--- src/main/java/com/turismo/model/Reserva.java
+++ src/main/java/com/turismo/model/Reserva.java
@@ -2,6 +2,7 @@ package com.turismo.model;
 
 
 import java.time.LocalDate;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -12,6 +13,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -30,105 +32,113 @@ public class Reserva {
 	private LocalDate  fecha_llegada;
 	@Column
 	private LocalDate  fecha_salida;
+	@Column
+	private String pago;
 	
 	@ManyToOne(fetch = FetchType.LAZY, optional = false  )
-	@JoinColumn(name = "cliente_rut", nullable=false)
+	@JoinColumn(name = "rut", nullable=false)
 	@JsonIgnore
     private Cliente cliente;
 	
 	@ManyToOne(fetch = FetchType.LAZY, optional = false  )
-	@JoinColumn(name = "servicio_id_servicio", nullable=false)
+	@JoinColumn(name = "id_servicio", nullable=false)
 	@JsonIgnore
     private Servicio servicio;
 	
 	@ManyToOne(fetch = FetchType.LAZY, optional = false  )
-	@JoinColumn(name = "depart_id_depart", nullable=false)
+	@JoinColumn(name = "id_depart", nullable=false)
 	@JsonIgnore
     private Depart depart;
 	
+	@OneToMany(mappedBy = "reserva", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Checkin> checkin;
 	
-	public Reserva() {}
-	
-	
+	@OneToMany(mappedBy = "reserva", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Checkout> checkout;
 
+	public Reserva() {
+		
+	}
 
 	public Reserva(int id_reserva, int precio_total, String descripcion, LocalDate fecha_llegada,
-			LocalDate fecha_salida) {
+			LocalDate fecha_salida, String pago, Cliente cliente, Servicio servicio, Depart depart,
+			Set<Checkin> checkin, Set<Checkout> checkout) {
 		super();
 		this.id_reserva = id_reserva;
 		this.precio_total = precio_total;
 		this.descripcion = descripcion;
 		this.fecha_llegada = fecha_llegada;
 		this.fecha_salida = fecha_salida;
-
+		this.pago = pago;
+		this.cliente = cliente;
+		this.servicio = servicio;
+		this.depart = depart;
+		this.checkin = checkin;
+		this.checkout = checkout;
 	}
-
-
-
 
 	public int getId_reserva() {
 		return id_reserva;
 	}
 
-
 	public void setId_reserva(int id_reserva) {
 		this.id_reserva = id_reserva;
 	}
-
 
 	public int getPrecio_total() {
 		return precio_total;
 	}
 
-
 	public void setPrecio_total(int precio_total) {
 		this.precio_total = precio_total;
 	}
-
 
 	public String getDescripcion() {
 		return descripcion;
 	}
 
-
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-
 
 	public LocalDate getFecha_llegada() {
 		return fecha_llegada;
 	}
 
-
 	public void setFecha_llegada(LocalDate fecha_llegada) {
 		this.fecha_llegada = fecha_llegada;
 	}
-
 
 	public LocalDate getFecha_salida() {
 		return fecha_salida;
 	}
 
-
 	public void setFecha_salida(LocalDate fecha_salida) {
 		this.fecha_salida = fecha_salida;
 	}
 
+	public String getPago() {
+		return pago;
+	}
 
+	public void setPago(String pago) {
+		this.pago = pago;
+	}
 
 	public Cliente getCliente() {
 		return cliente;
 	}
 
-
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
 
-
 	public Servicio getServicio() {
 		return servicio;
+	}
+
+	public void setServicio(Servicio servicio) {
+		this.servicio = servicio;
 	}
 
 	public Depart getDepart() {
@@ -139,6 +149,23 @@ public class Reserva {
 		this.depart = depart;
 	}
 
+	public Set<Checkin> getCheckin() {
+		return checkin;
+	}
+
+	public void setCheckin(Set<Checkin> checkin) {
+		this.checkin = checkin;
+	}
+
+	public Set<Checkout> getCheckout() {
+		return checkout;
+	}
+
+	public void setCheckout(Set<Checkout> checkout) {
+		this.checkout = checkout;
+	}
+
+	
 	
 	
 }
